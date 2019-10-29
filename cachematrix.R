f7e346a39987fa531afa8532fe045e4c163e67e0
sarahschirduan <sarah.schirduan@gmail.com>
2019-10-28 21:29:22 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The purpose of these two functions, makeCacheMatrix & cacheSolve is to cache
+# the inverse of a matrix
 
-## Write a short comment describing this function
+
+# The makeCacheMatrix function creates an R object that stores a matrix & its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
+# The cacheSolve function requires an argument that is returned by makeCacheMatrix() 
+# in order to retrieve the inverse from the cached value stored in the makeCacheMatrix() 
+# object's environment.
 
-## Write a short comment describing this function
+# This function is necessary because the first function does not result in returning
+# the inverse, it merely stores it 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  
 }
 
-##TESTING
+
+#Testing functions
+testmatrix <- matrix(c(4, 7, 2, 6), nrow = 2, ncol = 2)  
+testmatrix
+testmatrix2 <- makeCacheMatrix(testmatrix)
+cacheSolve(testmatrix2)
